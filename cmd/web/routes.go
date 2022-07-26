6f35fc9e746578cef87045c70af19aedd0e4eb4d
Francesco <francesco.varsallona@gmail.com>
2022-07-26 21:01:18 +0200

    Setup a login page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -32,6 +32,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/choose-room/{id}", handlers.Repo.ChooseRoom)
 	mux.Get("/book-room", handlers.Repo.BookRoom)
 
+	mux.Get("/user/login", handlers.Repo.ShowLogin)
+
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 

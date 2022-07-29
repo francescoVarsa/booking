30066c6b6027b2b0c658c135263ee5b9824d0b0f
Francesco <francesco.varsallona@gmail.com>
2022-07-29 23:57:08 +0200

    Making a reservation as processed

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -46,7 +46,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminShowReservation)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
-
+		mux.Post("/process-reservation/{src}/{id}", handlers.Repo.AdminProcessReservation)
 	})
 
 	fileServer := http.FileServer(http.Dir("./static/"))
